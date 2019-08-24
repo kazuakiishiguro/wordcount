cc222d018f9fed4150592b0e61df382286ce680a
Kazuaki Ishiguro <kazuaki@couger.co.jp>
2019-08-25 01:06:46 +0900

    added inline tests

--- src/lib.rs
+++ src/lib.rs
@@ -35,7 +35,7 @@ impl Default for CountOption {
 ///
 /// ```
 /// use std::io::Cursor;
-/// use bicycle_book_wordcount::{count, CountOption};
+/// use wordcount::{count, CountOption};
 ///
 /// let mut input = Cursor::new("aa bb cc bb");
 /// let freq = count(input, CountOption::Word);
@@ -71,4 +71,60 @@ pub fn count(input: impl BufRead, option: CountOption) -> HashMap<String, usize>
         }
     }
     freqs
+}
+
+#[test]
+fn word_count_works() {
+    use std::io::Cursor;
+    let mut exp = HashMap::new();
+    exp.insert("aa".to_string(), 1);
+    exp.insert("bb".to_string(), 2);
+    exp.insert("cc".to_string(), 1);
+
+    assert_eq!(count(Cursor::new("aa bb cc bb"), CountOption::Word), exp);
+}
+
+#[test]
+fn word_count_works2() {
+    use std::io::Cursor;
+    let mut exp = HashMap::new();
+    exp.insert("aa".to_string(), 1);
+    exp.insert("cc".to_string(), 1);
+    exp.insert("dd".to_string(), 1);
+
+    assert_eq!(count(Cursor::new("aa cc dd"), CountOption::Word), exp);
+}
+
+#[test]
+#[should_panic]
+fn word_count_do_not_contain_unknown_words() {
+    use std::io::Cursor;
+    count(
+        Cursor::new([
+            b'a',
+            0xf0, 0x90, 0x80,
+            0xe3, 0x81, 0x82,
+        ]),
+        CountOption::Word,
+    );
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use std::io::Cursor;
+
+    macro_rules! assert_map {
+        ($expr: expr, {$($key: expr => $value:expr),*}) => {
+            $(assert_eq!($expr[$key], $value));*
+        };
+    }
+
+    #[test]
+    fn word_count_works3() {
+        let freqs = count(Cursor::new("aa  cc dd"), CountOption::Word);
+
+        assert_eq!(freqs.len(), 3);
+        assert_map!(freqs, {"aa" => 1, "cc" => 1, "dd" => 1});
+    }
 }
